3aa1c1578e4957f5301fe93ec9f72943e3a09e7f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-12-23 14:28:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const containerVideos = document.querySelector(".videos__container");
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+const botoesCategorias = document.querySelectorAll(".superior__item");
+
+async function buscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+
+        videos.forEach((video) => {
+            if (video.categoria == "") {
+                throw new Error('Vídeo não tem categoria');
+            }
+            containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
+                    </div>
+                </li>
+                `;
+        })
+    } catch (error) {
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
+}
+
+function filtrarPesquisa() {
+    const listaVideos = document.querySelectorAll(".videos__item");
+    let valorFiltro = barraDePesquisa.value.toLowerCase();
+    
+    listaVideos.forEach(video => {
+        let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+        let possuiValorFiltrado = titulo.includes(valorFiltro);
+        /**
+         * Operação ternario 
+         */
+        video.style.display = valorFiltro ?
+            possuiValorFiltrado ?
+                'block' :
+                'none' :
+            'block';
+    })
+}
+
+function filtrarPorCategoria(filtro){
+    //debugger
+    const listaVideos  = document.querySelectorAll(".videos__item");
+    listaVideos.forEach(video => {
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+        console.log(`${categoria} == ${valorFiltro}`)
+
+        video.style.display = !categoria.includes(valorFiltro) && valorFiltro != 'tudo' ?
+            "none":
+            "block";
+    });
+}
+
+botoesCategorias.forEach(botao =>{
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", ()=> filtrarPorCategoria(nomeCategoria));
+})
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+buscarEMostrarVideos();
+
